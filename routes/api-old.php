69f980c843680174462c927dac803fcaaa1577f4
SamLiu2001 <samliu600@gmail.com>
2025-02-14 15:30:33 +0800

    API

--- routes/api-old.php
+++ routes/api-old.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReportController;
@@ -9,8 +8,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-
-Route::apiResource('reports', ReportController::class);
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/reports', [ReportController::class, 'store']);
+c
