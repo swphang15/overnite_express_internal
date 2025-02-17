7f1de4c96471170b55fb674c26e3231013194e8f
Aidan0641 <alnhuskar37@gmail.com>
2025-02-17 15:20:08 +0800

    all done

--- database/migrations/2025_02_14_080425_create_manifests_table.php
+++ database/migrations/2025_02_14_080425_create_manifests_table.php
@@ -10,27 +10,28 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up()
-{
-    Schema::create('manifest', function (Blueprint $table) {
-        $table->id();
-        $table->string('origin');
-        $table->string('consignor');
-        $table->string('consignee');
-        $table->string('cn_no');
-        $table->integer('pcs');
-        $table->integer('kg');
-        $table->integer('gram');
-        $table->text('remarks')->nullable();
-        $table->date('date');
-        $table->string('awb_no');
-        $table->string('to');
-        $table->string('from');
-        $table->string('flt');
-        $table->string('manifest_no');
-        $table->timestamps();
-    });
-}
-
+    {
+        Schema::create('manifests', function (Blueprint $table) {
+            $table->id();
+            $table->string('origin');
+            $table->foreignId('consignor_id')->constrained('companies')->onDelete('cascade');
+            $table->foreignId('consignee_id')->constrained('companies')->onDelete('cascade');
+            $table->integer('cn_no');
+            $table->integer('pcs');
+            $table->integer('kg');
+            $table->integer('gram');
+            $table->text('remarks')->nullable();
+            $table->date('date');
+            $table->integer('awb_no');
+            $table->string('to');
+            $table->string('from');
+            $table->string('flt');
+            $table->integer('manifest_no');
+            
+            $table->timestamps();
+        });
+    }
+    
 
     /**
      * Reverse the migrations.
