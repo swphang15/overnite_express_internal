0b5d3fcd19dcb9f490bb1a52a219fcb149692dc6
SamLiu2001 <samliu600@gmail.com>
2025-02-17 17:31:51 +0800

    ok

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ManifestController;
 
 
@@ -12,8 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::apiResource('manifest', ManifestController::class);
-Route::apiResource('reports', ReportController::class);
+Route::apiResource('manifests', ManifestController::class);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
