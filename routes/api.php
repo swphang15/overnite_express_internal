ccb5ae5f8343051195a13d33caec036357e7da80
Aidan0641 <alnhuskar37@gmail.com>
2025-02-21 10:49:59 +0800

    ok done

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ManifestController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\ShippingController;
+use App\Models\Agent;
+use App\Models\Company;
+
+
+Route::get('/export-excel', [ReportController::class, 'exportExcel']);
+Route::get('/export-pdf', [ReportController::class, 'exportPdf']);
+
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -14,9 +23,14 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 Route::post('/calculate-shipping', [ShippingController::class, 'calculateShipping']);
 
 
-Route::get('/companies', [CompanyController::class, 'index']);
-Route::get('/agents', [AgentController::class, 'index']);
 
+Route::get('/agents', function () {
+    return response()->json(Agent::all());
+});
+
+Route::get('/companies', function () {
+    return response()->json(Company::all());
+});
 Route::apiResource('manifests', ManifestController::class);
 Route::post('/manifests', [ManifestController::class, 'store']);
 
